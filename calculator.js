33eeac81dd87387782a1ee17dcd1f510aed12191
shujixx <otologie@gmail.com>
2016-04-19 14:13:51 +0900

    Added files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+
+var bfm = "", bk = 7;
+
+
+while (bk == 7) {
+var text = Editor.Inputbox("ŒvŽZŽ®", bfm, 200);
+
+if (text != "") {
+
+	var ans = eval(text);
+
+	bk = Editor.MessageBox( ans,"4");
+
+	if (bk == 7) bfm = text;
+}
+
+else bk = 6;
+
+}
+
+
